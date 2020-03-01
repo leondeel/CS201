062eb1d3c7dae7053c42d0e674ad62624993ac9f
Ariel Hernandez <leondeel@gmail.com>
2020-03-01 14:37:48 -0900

    made prototype for doesNameExist and working on the function design

--- Homework2/Source.cpp
+++ Homework2/Source.cpp
@@ -9,27 +9,26 @@ using std::string;
 using std::getline;
 using std::vector;
 
+void inputNames(vector<string>& names);
+bool doesNameExist(const string &nameToFind, const vector<string> &names);
 
-void InputNames(vector<string>& names);
-
-
-int main(int argc, char** argv) {
-
+int main() {
 	vector<string> names;
-
-	InputNames(names);
-
-	for (int i = 0; i < 10; i++) {
-		cout << names[i] << endl;
-	}
+	inputNames(names);
 	return 0;
 }
 
 
-void InputNames(vector<string> &names) {
+bool doesNameExist(const string& nameToFind, const vector<string>& names) {
+	for (int i = 0; i <= names.size(); i++) {
+
+	}
+	return 0;
+}
+
+void inputNames(vector<string> &names) {
 
 	for (int i = 0; i < 10; i++) {
-
 		string name;
 		cout << "Please enter a name:  ";
 		getline(cin, name);
