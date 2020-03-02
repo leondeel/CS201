edefdf49c8478b5230681e2fdb611816d5539a3b
Ariel Hernandez <leondeel@gmail.com>
2020-03-01 23:47:43 -0900

    The functions "doesNameExist" and "inputNames" are finally completed and debugged.

--- Homework2/Source.cpp
+++ Homework2/Source.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <algorithm>
 #include <string>
+
 using std::cout;
 using std::cin;
 using std::endl;
@@ -17,25 +18,22 @@ int main() {
 	inputNames(names);
 	return doesNameExist("Ariel", names);
 }
-
-bool doesNameExist(const string& nameToFind, const vector<string>& names) {
-
-	if (nameToFind.size() == 0) return 1; //Any name with zero letters is contained within any list of any kind.
-	for (unsigned int i = 0; i < names.size(); i++) {
-		for (unsigned int k = 0; k <= names[i].size() - nameToFind.size() && 0<= names[i].size() - nameToFind.size(); k++) {
-			for (unsigned int n = 0; names[i][k + n] == nameToFind[n] && n < nameToFind.size(); n++ ) {
-				if (n == nameToFind.size() - 1) return 1;			
-			}
-		}
-	}
-	return 0;
-}
-
-void inputNames(vector<string> &names) {
+void inputNames(vector<string>& names) {
 	for (int i = 0; i < 10; i++) {
 		string name;
 		cout << "Please enter a name:  ";
 		getline(cin, name);
 		names.push_back(name);
 	}
+}
+bool doesNameExist(const string& nameToFind, const vector<string>& names) {
+	if (nameToFind.size() == 0) return 1; //Any name with zero letters is contained within any list of any kind.
+	for (unsigned int i = 0; i < names.size(); i++) {
+		for (unsigned int k = 0; nameToFind.size() <= names[i].size() && k <= names[i].size() - nameToFind.size(); k++) {
+			for (unsigned int n = 0; names[i][k+n] == nameToFind[n] && n < nameToFind.size(); n++ ) {
+				if (n == nameToFind.size() - 1) return 1;			
+			}
+		}
+	}
+	return 0;
 }
