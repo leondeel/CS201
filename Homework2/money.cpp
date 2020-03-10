4a56092376d28cdb64ba4598d32e7f385e1c8656
Ariel Hernandez <leondeel@gmail.com>
2020-03-10 14:47:49 -0800

    money.cpp initial commit

---
+++ Homework2/money.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::getline;
+using std::vector;
+using std::min_element;
+
+void main() {
+	string pennies;
+	cout << "How many pennies do you have?" << endl;
+	getline(cin, pennies);
+
+}
+
--- Homework2/names.cpp
+++ Homework2/names.cpp
@@ -15,7 +15,7 @@ void inputNames(vector<string>& names);
 bool doesNameExist(const string &nameToFind, const vector<string> &names);
 void printNames(const vector<string>& names);
 void interesting(vector<string>& names);
-//This function sorts its input into dictionary-order, or rather into ASCII order or some equivalent from least to greatest.
+//This function sorts its input into dictionary-order, or rather into ASCII order or some equivalent like ASCII or Unicode from least to greatest.
 
 int main() {
 	vector<string> names;
@@ -50,7 +50,7 @@ void printNames(const vector<string>& names) {
 	}
 }
 
-//This function sorts its input into dictionary-order, or rather into ASCII order or some equivalent from least to greatest.
+//This function sorts its input into dictionary-order, or rather into ASCII order or some equivalent like ASCII or Unicode from least to greatest.
 void interesting(vector<string>& names) {
 	int vLength = names.size();
 	vector<string> names2(vLength);
