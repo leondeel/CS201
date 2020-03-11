65c2ff7a4d6be917300d19b7a8ee3e04840669d6
Ariel Hernandez <leondeel@gmail.com>
2020-03-10 23:58:15 -0800

    I forgot to commit again.  Please be merciful.  I still coded all this by myself.

--- Homework3/Source.cpp
+++ Homework3/Source.cpp
@@ -16,26 +16,38 @@ using std::istringstream;
 void printBox(const string& wordInBoxToPrint, const int & boxLayers);
 
 void main() {
-	string wordInBox, InputInteger;
-	int boxLayers;
-	cout << "Type a string you want in the box:  ";
-	getline(cin, wordInBox);
-	cout << "Type a non-negative integer for the number of layers you want the box to have:  ";
-	getline(cin, InputInteger);
-	istringstream instream(InputInteger);
-	instream >> boxLayers; // Read number from line
 
-	while (!instream || boxLayers < 0) // Error check
-	{
-		cout << "Please type an INTEGER that is NOT NEGATIVE for the number of layers you want the box to have:  ";
-		instream.clear();
-		instream.ignore();
+	string userContinues = "y";
+
+	while (userContinues[0] == 'y' || userContinues[0] == 'Y') {
+
+		string wordInBox, InputInteger;
+		int boxLayers;
+		cout << "Type a string you want in the box:  ";
+		getline(cin, wordInBox);
+		cout << "Type a non-negative integer for the number of layers you want the box to have:  ";
 		getline(cin, InputInteger);
 		istringstream instream(InputInteger);
-		instream >> boxLayers ; // Read number from line
+		instream >> boxLayers; // Read number from line
+
+		while (!instream || boxLayers < 0) // Error check
+		{
+			cout << "Please type an INTEGER that is NOT NEGATIVE for the number of layers you want the box to have:  ";
+			instream.clear();
+			instream.ignore();
+			getline(cin, InputInteger);
+			istringstream instream(InputInteger);
+			instream >> boxLayers; // Read number from line
+		}
+		cout << endl;
+		printBox(wordInBox, boxLayers);
+
+		instream.clear();
+		instream.ignore();
+
+		cout << "Do you wish to repeat that?  \nIf so, enter \"yes\". Otherwise, enter anything else that doesn't start with the letter \"y\"....   " << endl;
+		getline(cin, userContinues);
 	}
-	cout << endl;
-	printBox(wordInBox, boxLayers);
 }
 
 void printBox(const string& wordInBox, const int& boxLayers) {
