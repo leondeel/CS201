e728bc1ec36edcbcf445b2102d8b396235e96ee9
Ariel Hernandez <leondeel@gmail.com>
2020-03-11 18:53:02 -0800

    My code is getting bulky and some of it is being reused for other code.  So I made a new function.

--- Homework3/boxer.cpp
+++ Homework3/boxer.cpp
@@ -14,7 +14,9 @@ using std::min_element;
 using std::istringstream;
 
 void printBox(const string& wordInBoxToPrint, const int & boxLayers);
+int getUserInt();
 void boxer();
+
 
 void main() { 
 	
@@ -27,29 +29,15 @@ void boxer() {
 
 	while (userContinues[0] == 'y' || userContinues[0] == 'Y') {
 
-		string wordInBox, InputInteger;
-		int boxLayers;
+		string wordInBox;
 		cout << "Type a string you want in the box:  ";
 		getline(cin, wordInBox);
 		cout << "Type a non-negative integer for the number of layers you want the box to have:  ";
-		getline(cin, InputInteger);
-		istringstream instream(InputInteger);
-		instream >> boxLayers; // Read number from line
-
-		while (!instream || boxLayers < 0) // Error check
-		{
-			cout << "Please type an INTEGER that is NOT NEGATIVE for the number of layers you want the box to have:  ";
-			instream.clear();
-			instream.ignore();
-			getline(cin, InputInteger);
-			istringstream instream(InputInteger);
-			instream >> boxLayers; // Read number from line
-		}
+		int boxLayers = getUserInt();
 		cout << endl;
 		printBox(wordInBox, boxLayers);
 		cout << endl;
-		instream.clear();
-		instream.ignore();
+
 
 		cout << "Do you wish to repeat that?  \nIf so, enter \"yes\". Otherwise, enter anything else that doesn't start with the letter \"y\"....   " << endl;
 		getline(cin, userContinues);
@@ -88,4 +76,25 @@ void printBox(const string& wordInBox, const int& boxLayers) {
 		}
 		cout << endl;
 	}
+}
+
+int getUserInt() {
+	string InputInteger;
+	int boxLayers;
+	getline(cin, InputInteger);
+	istringstream instream(InputInteger);
+	instream >> boxLayers; // Read number from line
+
+	while (!instream || boxLayers < 0) // Error check
+	{
+		cout << "Please type an INTEGER that is NOT NEGATIVE:  ";
+		instream.clear();
+		instream.ignore();
+		getline(cin, InputInteger);
+		istringstream instream(InputInteger);
+		instream >> boxLayers; // Read number from line
+	}
+	instream.clear();
+	instream.ignore();
+	return boxLayers;
 }
