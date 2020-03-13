4f46e86a9c9155d525fc9a622c493c6fd137effd
Ariel Hernandez <leondeel@gmail.com>
2020-03-12 21:46:11 -0800

    This code you gave us to fix is so bad its funny.

--- Homework3/kelvin.cpp
+++ Homework3/kelvin.cpp
@@ -1,2 +1,14 @@
 #include "std_lib_facilities.h"
 
+double ctok(double c)
+{
+	int k = c + 273.15;
+	return int;
+}
+int main()
+{
+	double c = 0;
+	cin >> d;
+	double k = ctok("c");
+	Cout << k << '/n';	
+}
