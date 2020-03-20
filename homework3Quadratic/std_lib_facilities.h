f012692ab9517532d8085fd82ebf8451efff72e0
Ariel Hernandez <leondeel@gmail.com>
2020-03-20 19:08:12 -0400

    starting quadratic

---
+++ homework3Quadratic/std_lib_facilities.h
@@ -0,0 +1,72 @@
+
+
+#ifndef _STD_FACILITIES_H_
+#define _STD_FACILITIES_H_
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <sstream>
+#include <iomanip>
+#include <fstream>
+#include <map>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::getline;
+using std::vector;
+using std::min_element;
+using std::istringstream;
+
+
+inline void keepWindowOpen() {//I have to say that I can't see any bugs in the inline function.
+	string tmp;
+	cin.clear();
+	cout << "Press ENTER to exit the program." << endl;
+	getline(cin, tmp);
+}
+
+//Below lie definitions for my own functions used in this homework and even other future homework.
+inline  int getUserInt() {//This prompts the user for a positive integer and error-checks it.
+	string InputInteger;
+	int boxLayers;
+	cout << "Please enter a positive integer:  ";
+	getline(cin, InputInteger);
+	istringstream instream(InputInteger);
+	instream >> boxLayers; // Read number from line
+
+	while (!instream || boxLayers < 1) // Error check
+	{
+		cout << "Please enter a positive integer only:  ";
+		instream.clear();
+		instream.ignore();
+		getline(cin, InputInteger);
+		istringstream instream(InputInteger);
+		instream >> boxLayers; // Read number from line
+	}
+	instream.clear();
+	instream.ignore();
+	cout << "Thank you!" << endl;
+	return boxLayers;
+}
+
+//This prompts the user to choose between exiting the program or continuing.
+inline bool userWantsToExit() {
+
+	string userExit;
+	cout << "\n\nDo you wish to repeat that?  If so, enter \"yes\". \nIf not, enter anything else that doesn't start with the letter \"y\"....   " << endl;
+	getline(cin, userExit);
+	if (userExit[0] == 'Y' || userExit[0] == 'y') {
+		return false;
+	}
+	return true;
+}
+
+#endif // !_STD_FACILITIES_H_
+
