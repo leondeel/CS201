0addc5ae8b087c728213186a8571554adbdf8e6d
Ariel Hernandez <leondeel@gmail.com>
2020-03-20 20:02:22 -0400

    I coded everything correctly basically on the first try and I forgot to make commits in the process.  I promise this is all my own code made originally for this assignment and it was written very quickly.  I'm sorry I forgot to make more commits.

--- homework3Quadratic/quadratic.cpp
+++ homework3Quadratic/quadratic.cpp
@@ -1,18 +1,35 @@
 #include "std_lib_facilities.h"
 
 
-double quadratic(double a, double b, double c) {
-
+vector<double> quadratic(const double &a, const double &b, const double &c) {
+	double discriminant = b*b - 4 * a * c;
+	double T = -b / (2 * a);
+	//In this case the roots are complex and they are conjugates. 
+	//So we only need to communicate the real part and the positive version of the complex part to have all the information we need.
+		if (discriminant < 0) {
+			return {0, T, sqrt(abs(discriminant)) / (2 * a), };  //I will let 0 indicate that the roots are complex.
+	    }
+		else {//I will let 1 indicate the roots are real.
+			return { 1, T + sqrt(discriminant) / (2 * a), T - sqrt(discriminant) / (2 * a) };
+		}
 }
 
 
 void main() {
-	cout << "Let a*x^2 + b*x +c  be  a single-variable polynomial, with variable x, with coeffcients a,b,c.  Please define a,b,c." << endl;
-	cout << "a = ";
-	double a = getDouble();
-	cout << "b = ";
-	double b = getDouble();
-	cout << "c = ";
-	double c = getDouble();
-
+	do {
+		cout << "Let ( a*x^2 + b*x + c ) be  a single-variable polynomial, with variable x, with real coeffcients a,b,c.  Now, please define a,b,c." << endl;
+		cout << "a = ";
+		double a = getDouble();
+		cout << "b = ";
+		double b = getDouble();
+		cout << "c = ";
+		double c = getDouble();
+		vector<double> roots = quadratic(a, b, c);
+		if (roots[0] == 0) {
+			cout << "The roots of this polynomial are two complex conjugates." << endl;
+		}
+		else {
+			cout << "Your roots are " << roots[1] << " and " << roots[2] << endl;
+		}
+	} while (!userWantsToExit());
 }
