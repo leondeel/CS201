bf259a340ef077dc06c5b4369d28bf5fa41d4778
Ariel Hernandez <leondeel@gmail.com>
2020-04-28 16:29:06 -0400

    blah

--- Labs/Lab1/Lab1/floating_point_main.cpp
+++ Labs/Lab1/Lab1/floating_point_main.cpp
@@ -1,28 +1,31 @@
 /**
  * @file     L19_Floating_Point.cpp
- * @author   Student Name
+ * @author   M. Ariel Hernandez
  * @version  Jan 28, 2019
  * John Quan 
  *
  * Useful trigonometric functions.
  */
 
+
+#include "floating_point.hpp"	
 #include <iostream>
 #include <vector>
 // using sin, cos
 #include <cmath>
 
-#include "floating_point.hpp"	
+
+
 
 int main() {
 	// TODO Turn on "error on warnings" so this doesn't*****************
 	// compile.  Make it compile by using std::size_t
 	// You can comment this for loop out after that.
 	std::vector<int> v { 1, 2, 3, 4, 5 };
-	for (int i = 2; i < v.size(); ++i) {
+	for (std::size_t i = 2; i < v.size(); ++i) {
 		++v[i];
 		std::cout << v[i] << std::endl;
-	}
+	}	
 	std::cout << std::endl << std::endl;
 	// *****************************************************************
 
