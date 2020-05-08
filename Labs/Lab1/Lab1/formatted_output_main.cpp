8e67af13f05429c9ac6eb6966e1942a78e8c6aaf
Ariel Hernandez <leondeel@gmail.com>
2020-05-08 17:41:25 -0400

    blah

--- Labs/Lab1/Lab1/formatted_output_main.cpp
+++ Labs/Lab1/Lab1/formatted_output_main.cpp
@@ -16,6 +16,7 @@ using std::vector;
 
 int main() {
 
+	//Part 1 of Lab 20
 #if 0
 	vector<double> x = degreesToRadians(360);
 	
@@ -31,16 +32,17 @@ int main() {
 
 #endif
 
+//Addictional part of lab 20
 #if 1
 	vector<double> x = degreesToRadians(360);
 
-	cout << "degree" << "  " << "cos(degree)" << std::fixed << endl;
+	cout << "  x" << "    " << "exp(x)" << std::scientific << endl;
 	for (size_t i = 0; i < 181; i += 5) {
-		if (cos(x[i]) >= 0) {
-			cout << std::setw(3) << i << "    " << std::setw(8) << cos(x[i]) << endl;
+		if (exp(x[i]/10) >= 0) {
+			cout << std::setw(3) << i << "   " << std::setw(8) << exp(x[i]/10) << endl;
 		}
 		else {
-			cout << std::setw(3) << i << "   " << std::setw(8) << cos(x[i]) << endl;
+			cout << std::setw(3) << i << "  " << std::setw(8) << exp(x[i]/10) << endl;
 		}
 	}
 
