b1160cf2c472e4abaaf52be9ec41da1a5a1c6aed
Ariel Hernandez <leondeel@gmail.com>
2020-05-19 16:21:03 -0400

    tokenizer

--- Homework4/tokenizer/Source.cpp
+++ Homework4/tokenizer/Source.cpp
@@ -1,7 +1,16 @@
 #include "C:\Users\M. Ariel Hernandez\source\repos\cs201\Labs\lab21\my_standard_library.h";
 
 
+#if 1
+bool readLine(const string& str) {
+	string line;
+	getline(cin, line);
+	if (line == str) return true;
+	return false;
+}
+#endif
 void main() {
-
-
+	string line = "abc";
+	cout << readLine(line) << endl;
+	
 }
