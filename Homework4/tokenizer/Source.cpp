5f811b9a9a932c7d7d6f5a8279ce3ca122c047c5
Ariel Hernandez <leondeel@gmail.com>
2020-05-19 16:33:14 -0400

    tokenizer

--- Homework4/tokenizer/Source.cpp
+++ Homework4/tokenizer/Source.cpp
@@ -1,14 +1,19 @@
 #include "C:\Users\M. Ariel Hernandez\source\repos\cs201\Labs\lab21\my_standard_library.h";
 
 
-#if 1
+
 bool readLine(const string& str) {
-	string line;
-	getline(cin, line);
-	if (line == str) return true;
+	if (str != "") return true;
 	return false;
 }
+
+#if 1
+unsigned int stringToTokensWS(const string& input, const vector<string>& tokens) {
+
+}
 #endif
+
+
 void main() {
 	string line = "abc";
 	cout << readLine(line) << endl;
