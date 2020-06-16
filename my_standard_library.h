96663c80c95d440efce9738b95de11be4761e31c
Ariel Hernandez <leondeel@gmail.com>
2020-06-16 15:17:14 -0400

    standard library

--- my_standard_library.h
+++ my_standard_library.h
@@ -1,7 +1,12 @@
+#ifndef my_standard_library_header
+#define my_standard_library_header
+//#warning my_standard_library.h included
 
-#ifndef _MY_STANDARD_LIBRARY_
-#define _MY_STANDARD_LIBRARY_
-
+//Comment out this whole Section of includes when you generate your source units
+//If you don't the code file that you genereate will so huge and full functions
+//That it will be unreadable!
+//You are only interested in debugging you custom includes, so comment out all of
+//the C++ standard stuff
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -14,6 +19,7 @@
 #include <cstdlib>
 #include <cmath>
 
+
 using std::cout;
 using std::cin;
 using std::endl;
@@ -23,54 +29,10 @@ using std::vector;
 using std::min_element;
 using std::istringstream;
 
-//Below lie definitions for my own functions used in this homework and even other future homework.
 
-inline int getPosInt() {//This prompts the user for a positive integer and error-checks it.
-
-	int boxLayers;
-	string InputInteger;
-
-	while (true) // Error check
-	{
-		cout << "Please type a positive integer only:  ";
-		getline(cin, InputInteger);
-		istringstream instream(InputInteger);
-		instream >> boxLayers; // Read number from line
-		if (instream && boxLayers > 0) break;
-	}
-	return boxLayers;
-}
-
-//This prompts the user to choose between exiting the program or continuing.
-inline bool userContinues() {
-
-	string userExit;
-	cout << "\n\nDo you wish to repeat that?  If so, enter \"yes\". " << endl
-		<< "Otherwise, if you wish to exit, press enter, or enter anything else that doesn't start with the letter \"y\"....   " << endl;
-	getline(cin, userExit);
-	if (userExit[0] == 'Y' || userExit[0] == 'y') {
-		return true;
-	}
-	return false;
-}
+int getPosInt();
+bool userContinues();
+long double getDouble();
 
 
-inline long double getDouble()
-{
-	long double outputDouble;
-	string inputDouble;
-	while (true) // Error check
-	{
-		cout << "Please correctly enter a double." << endl;
-		getline(cin, inputDouble);
-		istringstream instream(inputDouble);
-		instream >> outputDouble; // Read number from line
-		if (instream) break;
-	}
-
-	cout << "Thank you!  You entered " << outputDouble << endl;
-	return outputDouble;
-
-}
-
-#endif // _MY_STANDARD_LIBRARY_
+#endif
