788b01f015592d6f4187f4cae942e1a5fe15bb8c
Ariel Hernandez <leondeel@gmail.com>
2020-06-16 15:14:56 -0400

    standard library

---
+++ my_standard_library.cpp
@@ -0,0 +1,51 @@
+#include "C:\Users\M. Ariel Hernandez\source\repos\cs201\my_standard_library.h"
+
+//Below lie definitions for my own functions used in this homework and even other future homework.
+
+int getPosInt() {//This prompts the user for a positive integer and error-checks it.
+
+	int boxLayers;
+	string InputInteger;
+
+	while (true) // Error check
+	{
+		cout << "Please type a positive integer only:  ";
+		getline(cin, InputInteger);
+		istringstream instream(InputInteger);
+		instream >> boxLayers; // Read number from line
+		if (instream && boxLayers > 0) break;
+	}
+	return boxLayers;
+}
+
+//This prompts the user to choose between exiting the program or continuing.
+bool userContinues() {
+
+	string userExit;
+	cout << "\n\nDo you wish to repeat that?  If so, enter \"yes\". " << endl
+		<< "Otherwise, if you wish to exit, press enter, or enter anything else that doesn't start with the letter \"y\"....   " << endl;
+	getline(cin, userExit);
+	if (userExit[0] == 'Y' || userExit[0] == 'y') {
+		return true;
+	}
+	return false;
+}
+
+
+long double getDouble()
+{
+	long double outputDouble;
+	string inputDouble;
+	while (true) // Error check
+	{
+		cout << "Please correctly enter a double." << endl;
+		getline(cin, inputDouble);
+		istringstream instream(inputDouble);
+		instream >> outputDouble; // Read number from line
+		if (instream) break;
+	}
+
+	cout << "Thank you!  You entered " << outputDouble << endl;
+	return outputDouble;
+
+}
