44c207a3a0b1d8acd61b6bed6fb6a692f092c68f
Ariel Hernandez <leondeel@gmail.com>
2020-06-21 12:57:45 -0400

    database

--- Homework4/database/main.cpp
+++ Homework4/database/main.cpp
@@ -8,7 +8,6 @@ struct DriverLicense {
 	char gender = 'O';
 
 };
-
 map<string, DriverLicense> theDatabase;
 
 #if 0
@@ -19,7 +18,7 @@ bool deleteRecord(const string& key);
 bool inputRecord(DriverLicense& record);
 bool printRecord(const string& key);
 #endif
-
+#if 1
 bool createRecord(const string& key) {
 	if (theDatabase.count(key) == 1 ) return false;
 	theDatabase[key];
@@ -30,7 +29,26 @@ bool readRecord(const string& key, DriverLicense& record) {
 	record = theDatabase[key];
 	return true;
 }
+bool updateRecord(const string& key, const DriverLicense& record) {
+	if (theDatabase.count(key) == 0) return false;
+	theDatabase[key] = record;
+	return true;
+}
+bool deleteRecord(const string& key) {
+	if (theDatabase.count(key) == 0) return false;
+	theDatabase.erase(key);
+	return true;
+}
+// Instructions for inputRecord are confusing.  Am I supposed to input a new record into the database, 
+// or am I supposed to extract a record from the databased and input it into a user-defined place?
+// If I am supposed to input a new record into the database, then what is its key supposed to be, 
+// and why is the function parameter not a const like in the other functions?  Doesn't the function updateRecord do this anyway?
+// If I am supposed to extract a record from the database into the function parameter, why isn't a "key" given as a parameter?
+// Doesn't readRecord do this anyway?  What's going on here?
+bool inputRecord(DriverLicense& record) {
 
+}
+#endif
 int main() {
 	createRecord("key");
 	if (theDatabase.count("key") == 1) {
