069b9c19cd9967440ddae37f5997f5eccb0b0db2
Ariel Hernandez <leondeel@gmail.com>
2020-06-22 17:39:53 -0400

    bullsAndCows

---
+++ Homework4/BullsAndCows/main.cpp
@@ -0,0 +1,14 @@
+#include "C:\Users\M. Ariel Hernandez\source\repos\cs201\my_standard_library.h"
+
+#include <random>
+using std::mt19937;
+
+
+
+int main(){
+
+	mt19937 gen; // PRNG object
+
+
+	return 0;
+}
