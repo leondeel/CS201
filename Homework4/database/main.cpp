54c104e82b150222c10ed23e714d86c9c8c8c92d
Ariel Hernandez <leondeel@gmail.com>
2020-06-22 12:24:12 -0400

    database

--- Homework4/database/main.cpp
+++ Homework4/database/main.cpp
@@ -10,7 +10,7 @@ map<string, DriverLicense> theDatabase;
 
 bool createRecord(const string& key) {
 	if (theDatabase.count(key) == 1 ) return false;
-	theDatabase[key];
+	theDatabase[key] = {"a", "b", "c", "d"};
 	return true;
 }
 bool readRecord(const string& key, DriverLicense& record) {
@@ -66,8 +66,9 @@ string getkey() {
 	string key;
 	bool contin = false;
 	do {
-		cout << "Enter a string with no white spaces:  ";
-		getline(cin, key);
+		do {
+			cout << "Enter a string with no white spaces:  ";
+		} while (!(readLine(key)));
 		for (auto n : key)
 		{
 			if (
@@ -102,44 +103,57 @@ int main() {
 			);
 		cout << endl;
 		if (key[0] == 'c') {
-			cout << "Alright! So you wish to CREATE a new record." << endl
-				<< "Please enter a Driver's License Number (or Driver's License String) for the new record. "<<endl;
+			cout << "Alright! So you wish to CREATE a new record." << endl;
 				do {
+					cout<< "Please enter a Driver's License Number (or Driver's License String) for the new record. " << endl;
 					key = getkey();
 					if (createRecord(key)) {
-						inputRecord(record);
-						cout << "A new record has succesfully been created with the License String: "<< key << endl;
+						cout <<endl<<
+							"You have sucessfuly entered a Driver's License String. Now, do you wish to CREATE a record from " << endl << 
+							"a copy of a previously READ record, or do you wish to write it from scratch?" << endl <<
+							"If you wish to write it from scratch, enter \"yes\", " << endl <<
+							"else, if you wish to CREATE with a copy (assuming you've already made a copy by using READ), " << endl <<
+							"enter anything else that doesn't start with the letter 'y'." << endl;
+
+						if (userYesNo) {
+							inputRecord(record);
+							updateRecord(key, record);
+							cout << "A new record has succesfully been created with the License String: " << key << endl << endl;
+						}
+						else {
+							updateRecord(key, record);
+							cout << "The record has been successfully updated. Note that if the copy was blank, so is this record now." << endl << endl;
+						}
 						break;
 					}
-					cout << "That License String already exists! Are you done trying to create a new record? ";
+					cout <<endl<< "That License String already exists! Are you done trying to create a new record? ";
 				} while (userContinues());
 		}
 		else if (key[0] == 'r') {
-			cout << 
-				"Alright! So you wish to READ a record. "<<endl<<
-				"(Or you wish to just find a record with a matching key, or copy it to UPDATE another by overwriting it later.)" 
-				<< endl << "Please enter a Driver's License Number (or Driver's License String) to search for. " << endl;
+			cout <<
+				"Alright! So you wish to READ a record. " << endl <<
+				"(Or you wish to just find a record with a matching key, " << endl <<
+				"or copy it to UPDATE another by overwriting the other later with what you just copied.)"
+				<< endl;
 			do {
+				cout << "Please enter a Driver's License Number (or Driver's License String) to search for. " << endl;
 				key = getkey();
 				if (readRecord(key, record)) {
-					cout << 
-						"A record with that license string exists.  This record has now been copied, "<<
-						"so you can overwrite another record by doing UPDATE, if you want. "<<endl<<
+					cout << endl <<
+						"We have found the record with that License String. This record has now been copied, " <<endl<<
+						"so you can overwrite another record by doing UPDATE, if you want. " << endl <<
 						"Here is what the record you searched for contains: " << endl;
 					printRecord(key);
-
-					theDatabase[key].fullName = "mah";   //  How come this works but printRecord doesn't????
-					cout << theDatabase[key].fullName << endl;//  Why?  what's going on here?
 				}
 				else {
-					cout << "That License String doesn't exist! ";
+					cout <<endl<< "That License String doesn't exist! ";
 				}
-				cout << "Are you done searching and reading records? ";
+				cout << "Are you done searching for and reading records? ";
 			} while (userContinues());
 		}
 		else if (key[0] == 'u') {
-			cout << "Alright! So you wish to UPDATE a record! Now, do you wish to UPDATE a record by overwriting it from a copy of" <<
-				" a previously READ record, or do you wish to re-write it from scratch?" << endl <<
+			cout << "Alright! So you wish to UPDATE a record! Now, do you wish to UPDATE a record by overwriting "<<endl<<
+				"it from a copy of a previously READ record, or do you wish to re-write it from scratch?" << endl <<
 				"If you wish to rewrite it from scratch, enter \"yes\", " << endl <<
 				"else, if you wish to UPDATE with a copy (assuming you've already made a copy by using READ), " << endl <<
 				"enter anything else that doesn't start with the letter 'y'." << endl;
@@ -150,10 +164,10 @@ int main() {
 				do {
 					key = getkey();
 					if (updateRecord(key, record)) {
-						cout << "The record has been successfully updated." << endl;
+						cout <<endl<< "The record has been successfully updated." << endl;
 						break;
 					}
-					cout << "That License String doesn't exist! Are you sure you want to UPDATE a record?" << endl;
+					cout <<endl<< "That License String doesn't exist! Are you sure you want to UPDATE a record?" << endl;
 				} while (userContinues());
 			}
 			else {
@@ -161,7 +175,8 @@ int main() {
 				do {
 					key = getkey();
 					if (updateRecord(key, record)) {
-						cout << "The record has been successfully updated. Note that if the copy was blank, so is this record now." << endl;
+						cout <<endl<< "The record has been successfully updated. Note that if the copy was blank, so is this record now." 
+							<< endl<<endl;
 						break;
 					}
 					cout << "That License String doesn't exist! Are you sure you want to UPDATE a record?" << endl;
@@ -174,7 +189,7 @@ int main() {
 			do {
 				key = getkey();
 				if (deleteRecord(key)) {
-					cout << "The record has been successfully deleted." << endl;
+					cout << endl<<"The record has been successfully deleted." << endl;
 					break;
 				}
 				cout << "That License String doesn't exist! Do you still wish to DELETE a record? Yes or no? ";
