f1b5266a2202b8711abe69838065f4e3ca5e1ea7
Ariel Hernandez <leondeel@gmail.com>
2020-06-22 18:00:06 -0400

    bullsAndCows

--- Homework4/BullsAndCows/main.cpp
+++ Homework4/BullsAndCows/main.cpp
@@ -1,13 +1,17 @@
 #include "C:\Users\M. Ariel Hernandez\source\repos\cs201\my_standard_library.h"
-
 #include <random>
-using std::mt19937;
-
 
 
 int main(){
+									
+	std::random_device seed;		// Source of RANDOM seed.
 
-	mt19937 gen; // PRNG object
+	std::mt19937 generator(seed()); // Generator for pseudo-random numbers.  
+									// The generator is only seeded once at the begninning of main.
+
+	std::uniform_int_distribution<int> distribution(0, 9); //"distribution" will behave as a function that has "generator" as
+														   // a parameter, and the "distribution" will RETURN a random integer from 0 to 9.
+
 
 
 	return 0;
