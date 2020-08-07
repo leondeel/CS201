a79658cff0f64ad84ef5f7aeed72150f615f6507
Ariel Hernandez <leondeel@gmail.com>
2020-08-07 19:55:08 -0400

    fltk

--- Homework5/FLTK/Source.cpp
+++ Homework5/FLTK/Source.cpp
@@ -10,12 +10,12 @@
 
 int main(int argc, char** argv) {
     Fl_Window* window = new Fl_Window(680, 360);
-    Fl_Box* box = new Fl_Box(0, 0, 100, 33, "Hello, World!");
+    Fl_Box* box = new Fl_Box(0, 0, 200, 33, "Instructions go here.");
     box->box(FL_UP_BOX);
     //box->labelfont(FL_BOLD + FL_ITALIC);
     box->labelsize(14);
     //box->labeltype(FL_SHADOW_LABEL);
-
+    Fl_Input* stringInput = new Fl_Input(0, 33, 200, 33);
 
 
 
