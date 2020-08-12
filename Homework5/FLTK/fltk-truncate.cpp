45e29b7174d4564b210742dd7b74e02a59e855cf
Ariel Hernandez <leondeel@gmail.com>
2020-08-12 16:09:56 -0400

    fltk

--- Homework5/FLTK/fltk-truncate.cpp
+++ Homework5/FLTK/fltk-truncate.cpp
@@ -1,24 +1,72 @@
 #pragma comment(lib, "fltk.lib")
 #pragma comment(lib, "comctl32.lib")
 
-#include <FL\Fl_Button.H>
-#include <FL\Fl.H>
-#include <FL\Fl_Window.H>
-#include <FL\Fl_Box.H>
-#include <FL\Fl_Input.H>
-#include "..\..\my_standard_library.h"
-#include <FL\Enumerations.H> 
-#include <FL\Fl_Input_.H>
+#include <FL/Fl_Button.H>
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Box.H>
+#include <FL/Fl_Input.H>
+#include "../../my_standard_library.h"
+#include <FL/Enumerations.H> 
+#include <FL/Fl_Input_.H>
+
+
+Fl_Window* window = new Fl_Window(680, 360);
+
+const int dx = 200;
+const int dy = 33;
+
+Fl_Box infoBox(0, 0, dx, dy, "Instructions go here.");
+Fl_Input stringInput(0, 1 * dy, dx, dy);
+Fl_Button truncateButton(0, 2 * dy, 2 * dx, dy, "Click to display the text you entered.");
+Fl_Button quitButton(0, 3 * dy, dx, dy, "Quit.");
+Fl_Box outputBox(0, 4 * dy, dx, dy, "Output goes here.");
 
 static void myQuit(Fl_Widget* w, void* userData) {
     exit(1);
 }
 #if 1
+static void display_input(Fl_Widget* w, void* userData)
+{
+    cout << endl << stringInput.value() << endl;
 
-static void copyInput(Fl_Widget* w, void* userData) {
-   cout << endl << &userData << endl << endl;
 }
+
 #endif
+
+int main(int argc, char** argv) {
+
+    truncateButton.callback(display_input);
+    quitButton.callback(myQuit);
+    // The function I defined above main() is myQuit.  That function gets passed to this so-called
+    // "callback function" which is a member function of the object quitButton so that the function myQuit
+    // is run every time the user clicks on that button which corresponds to quitButton.
+
+    window->end();
+    window->show(argc, argv);
+    return Fl::run();
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#if 0
+static void myQuit(Fl_Widget* w, void* userData) {
+    exit(1);
+}
+
+
 
 int main(int argc, char** argv) {
 
@@ -42,3 +90,4 @@ int main(int argc, char** argv) {
     return Fl::run();
 
 }
+#endif
