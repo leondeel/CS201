f3d76b7e4b1b670a3f3597cf2ce05e9769e0e80e
Ariel Hernandez <leondeel@gmail.com>
2020-08-12 13:28:47 -0400

    fltk

--- Homework5/FLTK/fltk-truncate.cpp
+++ Homework5/FLTK/fltk-truncate.cpp
@@ -13,33 +13,32 @@
 static void myQuit(Fl_Widget* w, void* userData) {
     exit(1);
 }
-#if 0
-string copyInput(Fl_Widget* w, void* userData) {
-    w->Fl_Input_::value();
+#if 1
+
+static void copyInput(Fl_Widget* w, void* userData) {
+   cout << endl << &userData << endl << endl;
 }
 #endif
 
 int main(int argc, char** argv) {
 
-    const int dx = 200;
-    const int dy = 33;
-    string sample = "sample text";
-    char s[4] = { 's', 'a', 'm', '\0' };
-    char* ps = &s[0];
-
     Fl_Window* window = new Fl_Window(680, 360);
 
+    const int dx = 200;
+    const int dy = 33;
 
     Fl_Box infoBox (0, 0, dx, dy, "Instructions go here.");
- 
     Fl_Input stringInput(0, 1*dy, dx, dy);
-    Fl_Input stringInput2(0, 2 * dy, dx, dy);
-    Fl_Button truncateButton(0, 3 * dy, dx, dy, "Truncate.");
-    Fl_Button quitButton(0, 4 * dy, dx, dy, "Quit.");
-    quitButton.callback(myQuit);
-    Fl_Box outputBox(0, 5 * dy, dx, dy, "Output goes here.");
+    Fl_Button truncateButton(0, 2 * dy, 2*dx, dy, "Click to display the text you entered.");
+    Fl_Button quitButton(0, 3 * dy, dx, dy, "Quit.");
+    quitButton.callback(myQuit); 
+    // The function I defined above main() is myQuit.  That function gets passed to this so-called
+    // "callback function" which is a member function of the object quitButton so that the function myQuit
+    // is run every time the user clicks on that button which corresponds to quitButton.
+    Fl_Box outputBox(0, 4 * dy, dx, dy, "Output goes here.");
 
     window->end();
     window->show(argc, argv);
     return Fl::run();
+
 }
