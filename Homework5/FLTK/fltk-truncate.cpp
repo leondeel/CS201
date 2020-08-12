3df44e6a288ae43e3e98d58d5f9ccaa7d095888e
Ariel Hernandez <leondeel@gmail.com>
2020-08-12 16:50:02 -0400

    fltk

--- Homework5/FLTK/fltk-truncate.cpp
+++ Homework5/FLTK/fltk-truncate.cpp
@@ -1,21 +1,17 @@
 #pragma comment(lib, "fltk.lib")
 #pragma comment(lib, "comctl32.lib")
-
 #include <FL/Fl_Button.H>
 #include <FL/Fl.H>
 #include <FL/Fl_Window.H>
 #include <FL/Fl_Box.H>
 #include <FL/Fl_Input.H>
 #include "../../my_standard_library.h"
-#include <FL/Enumerations.H> 
 #include <FL/Fl_Input_.H>
 #include <FL/Fl_Output.H>
 
 Fl_Window* window = new Fl_Window(680, 360);
-
 const int dx = 200;
 const int dy = 33;
-
 Fl_Box infoBox(0, 0, dx, dy, "Instructions go here.");
 Fl_Input stringInput(0, 1 * dy, dx, dy);
 Fl_Button truncateButton(0, 2 * dy, 2 * dx, dy, "Click to display the text you entered.");
@@ -25,25 +21,17 @@ Fl_Output outputBox(0, 4 * dy, dx, dy);
 static void myQuit(Fl_Widget* w, void* userData) {
     exit(1);
 }
-#if 1
-static void display_input(Fl_Widget* w, void* userData)
-{
+static void display_input(Fl_Widget* w, void* userData){
     outputBox.value(stringInput.value());
-
 }
-
-#endif
-
-int main(int argc, char** argv) {
-
+int main() {
     truncateButton.callback(display_input);
     quitButton.callback(myQuit);
-    // The function I defined above main() is myQuit.  That function gets passed to this so-called
-    // "callback function" which is a member function of the object quitButton so that the function myQuit
-    // is run every time the user clicks on that button which corresponds to quitButton.
-
+   // This uses the function I defined above main(), called myQuit.  That function gets passed to this so-called
+   // "callback function" which is a member function of the object quitButton so that the function myQuit
+   // is run every time the user clicks on that button which corresponds to quitButton.
+   // What I just explained also applies to truncateButton.callback(display_input)
     window->end();
-    window->show(argc, argv);
+    window->show();
     return Fl::run();
-
 }
